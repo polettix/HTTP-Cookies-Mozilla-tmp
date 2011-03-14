8e1ab30218ef282b4d1192d4414f664468eba1fa
Flavio Poletti <flavio@polettix.it>
2011-03-14 23:27:00 +0100

    bumped version

--- lib/Mozilla.pm
+++ lib/Mozilla.pm
@@ -84,7 +84,7 @@ use Carp qw(carp);
 use constant TRUE  => 'TRUE';
 use constant FALSE => 'FALSE';
 
-$VERSION = '2.02';
+$VERSION = '2.03';
 $SQLITE = 'sqlite3';
 
 my $EPOCH_OFFSET = $^O eq "MacOS" ? 21600 : 0;  # difference from Unix epoch
